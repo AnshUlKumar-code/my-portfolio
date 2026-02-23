97481b7361bb3ce76069f3889433a3eb5a5d7249
Anshul Kumar <anshulkumar73610@gmail.com>
2026-02-24 04:09:21 +0530

    fixed cors

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: [process.env.FRONTEND_URL ||  'https://my-portfolio-nine-xi-78.vercel.app'],
   credentials: true
 }))
 
